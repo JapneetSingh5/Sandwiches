ecf3e6e90fed7e4d2e8ee1a186960417228ce223
Japneet Singh <singhjapneetssa@gmail.com>
2020-06-23 23:03:35 +0530

    Initial Commit

---
+++ Sandwiches/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sandwiches
+//
+//  Created by Japneet Singh on /236/20.
+//  Copyright © 2020 Japneet Singh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
